8807bd2ed35149427134e242d462516772c6d325
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-20 15:12:53 +0000

    Save progress: 1047.1

--- insert_data.sh
+++ insert_data.sh
@@ -55,7 +55,12 @@ do
     #get major_id 
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     echo $MAJOR_ID
+
     #if not found 
+    if [[ -z $MAJOR_ID ]]
+    then
+      #set to null 
+    fi
     #set to null 
     #insert student
   fi
