18d4bf696784f3d8c873f40238011cc7bbf4a587
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-20 15:24:05 +0000

    Save progress: 1130.1

--- insert_data.sh
+++ insert_data.sh
@@ -48,7 +48,7 @@ do
     fi
   fi
 done
-cat students_test.csv | while IFS="," read FIRST LAST MAJOR GPA
+cat students.csv | while IFS="," read FIRST LAST MAJOR GPA
 do
   if [[ $FIRST != first_name ]]
   then
