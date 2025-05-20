3bf585311196d94665b255bc9e862ae85f2f6952
Tamyres silvazz <tamyressilvazz@outlook.com>
2025-05-20 14:52:59 +0000

    Save progress: 910.1

--- insert_data.sh
+++ insert_data.sh
@@ -41,5 +41,6 @@ do
 
     # insert into majors_courses
     COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+    INSERT_MAJORS_COURSES_RESULT=$($PSQL "INSERT INTO majors_courses(major_id, course_id) VALUES($MAJOR_ID, $COURSE_ID)")
   fi
 done
